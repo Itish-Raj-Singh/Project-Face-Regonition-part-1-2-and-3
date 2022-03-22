9852f108eea59b7d2d6cccf605c05f9957bd8257
Itish-Raj-Singh <94170586+Itish-Raj-Singh@users.noreply.github.com>
2022-03-22 15:39:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,37 +1,59 @@
+prediction = "";
+
 Webcam.set({
-    width:350,
-    height:300,
-    image_format:"png",
-    png_quality:90
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot() {
+    Webcam.snap(function (data_uri){
+        document.getElementById("result").innerHTML = '<img id="image_captured" src="'+data_uri+'"/>';
     });
-    
-     Webcam.attach('#camera');
-    
-     function take_snapshot(){
-       Webcam.snap(function(data_uri){
-           document.getElementById("result").innerHTML ='<img id="captured_image" src="'+data_uri+'"/>';
-       })
-     }
-    
-     console.log("ml5 version" , ml5.version);
-    
-    camera = document.getElementById("camera");
-    
-    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/3aTnxnbrI/model.json',modelloaded)
-function modelloaded(){
-    console.log("modelloaded");
 }
-function check(){
-    img = document.getElementById('captured_image');
-    classifier.classify(img , gotResult);
-  }
-   function gotResult(error , result){
-     if(error){
-     console.error(error);
+
+console.log("ml5 version:",ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/MwR4F891o/model.json',modelLoaded);
+
+function modelLoaded() {
+    console.log("Model Loaded Successfully!");
+}
+
+function speak() {
+    var synth = window.speechSynthesis;
+    var speak_data = "The Prediction Is "+prediction;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+}
+function check() {
+    img = document.getElementById("results");
+    classifier.classify(img, gotResults);
+}
+
+function gotResults(error, results){
+    if(error){
+        console.error(error);
     }
     else{
-      console.log(result);
-      document.getElementById("results_of_object").innerHTML = result[0].label;
-      document.getElementById("results_of_Accuracy").innerHTML= result[0].confidence.toFixed(3);
-    }
+        console.log(results);
+        document.getElementById("result_gesture_name").innerHTML = results[0].label;
+        prediction = results[0].label;
+        speak();
+        if(results[0].label == "Amazing"){
+            document.getElementById("result_emoji").innerHTML = "&#128076;";
+        }
+        if(results[0].label == "Best"){
+            document.getElementById("result_emoji").innerHTML = "&#128077";
+        }
+        if(results[0].label == "Victory"){
+            document.getElementById("result_emoji").innerHTML =' &#9996;';
+        }
+     
+        }
     }
